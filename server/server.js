fd8ace39f7247b238384714d710e219d8ddc203d
mugdhasingh25 <manyasingh2501@gmail.com>
2026-03-06 03:39:21 +0530

    Set body size limits for JSON and URL-encoded data
    Increase JSON and URL-encoded body size limits for requests.

--- server/server.js
+++ server/server.js
@@ -14,7 +14,8 @@ await connectDB()
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
 
 app.get('/', (req, res)=> res.send("Server is running"))
 app.use('/api/user', userRouter)
