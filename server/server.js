b6d9315c6f30e4a0d06ab90c2ff2f5e155fe97b3
aloksharma15 <128844817+aloksharma15@users.noreply.github.com>
2026-03-14 16:24:02 +0530

    Fix production CORS origin

--- server/server.js
+++ server/server.js
@@ -17,8 +17,8 @@ await connectDB()
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+  origin: [process.env.CLIENT_URL],
+  credentials: true
 }));
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb", extended: true }));
